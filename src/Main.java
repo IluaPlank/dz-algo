51192affceeeb6033a3585d4084bc8e28e390920
IluaPlank <gerevan1994@gmail.com>
2021-11-02 16:49:07 +0500

    не то искал

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,25 @@ public class Main {
 
     public static void placeBook (int book,int [] bookshelf){
         int left = 0;
-        int right = bookshelf.length;
+        int right = bookshelf.length-1;
         while(left < right) {
             int middle = (left + right) / 2;
             if (bookshelf[middle] == book){
                 if(bookshelf[middle+1] > book ){
                     left=middle+1;
                 }
-                else left=middle-1;
+                else{
+                    left=middle-1;
+                }
             }
             // если средний элемент меньше,идём вправо
-            if (bookshelf[middle] < book)
+            if (bookshelf[middle] < book){
                 left = middle + 1;
+            }
             // если средний элемент больше,идём влево
-            if (bookshelf[middle] > book)
+            if (bookshelf[middle] > book){
                 right = middle - 1;
+            }
         }
         System.out.println(bookshelf.length-left);
     }
